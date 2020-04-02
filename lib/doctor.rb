53a632f111cab19f522eaa6ffa9c1374abca429b
theluispestana <theluispestana@gmail.com>
2020-04-02 18:45:46 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+require "pry.rb"
+
+class Doctor 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select { |app| app.doctor == self }
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.map { |app| app.patient }
+  end
+end
