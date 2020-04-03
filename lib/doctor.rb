69988adb4830654a69c80f3bb3ebfd209396d692
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-02 19:02:22 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    Doctor.all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appt| appt.doctor == self }
+  end
+
+  def patients
+    appointments.map { |appt| appt.patient}
+  end
+
+end
