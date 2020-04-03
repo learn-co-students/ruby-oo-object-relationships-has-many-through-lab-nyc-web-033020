9ee9ed49820b16ac0e3c299cd04f3dd2eff6c71c
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-04-02 22:55:06 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+      @@all << self
+    end
+
+    
+
+
+
+end
