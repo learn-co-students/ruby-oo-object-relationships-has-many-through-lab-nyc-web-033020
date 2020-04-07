eb3927c963e305856fdada526022cfa95c7159de
coralfussman <coralfussman@gmail.com>
2020-04-07 08:46:57 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+    attr_reader :name, :artist, :genre    
+    @@ all = []
+    
+        def initialize(name, artist, genre)
+            @name = name
+            @@all << self
+        end
+    
+        def self.all
+            @@all
+        end
+    
+    end
+    
