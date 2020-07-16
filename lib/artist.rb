fb62c61ed8c58f5f4132a150aa22b3c4b0c0135c
junko911 <junkotahara911@gmail.com>
2020-07-16 19:58:58 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,4 +10,16 @@ class Artist
     def self.all
         @@all
     end
+
+    def songs
+        Song.find_by_artist(self)
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres
+        songs.map {|song| song.genre}
+    end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,19 @@
-    
+class Song
+    attr_reader :artist, :genre
+    @@all = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        Song.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_artist(artist)
+        all.select { |song| song.artist == artist }
+    end
+end
